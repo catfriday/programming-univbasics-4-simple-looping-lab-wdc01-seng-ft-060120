8cf5a5a3fa18caa89a7756b1a998795e41f8ce70
Catrina Friday <cat.friday@gmail.com>
2020-04-30 20:38:48 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -16,3 +16,16 @@ def loop_message_n_times(message,limit)
     count+=1 
   end
 end 
+
+def output_array(array)
+  counter=0 
+  while array[counter] do
+    puts array [counter]
+    counter+=l 
+  end
+end 
+  
+
+
+
+
